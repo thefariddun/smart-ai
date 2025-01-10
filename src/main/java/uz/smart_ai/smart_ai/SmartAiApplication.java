ab473a8e9a8e91e0e71b98fef708ee467c41b0cf
thefariddun <thefariddun@gmail.com>
2025-01-10 17:20:27 +0500

    Init

---
+++ src/main/java/uz/smart_ai/smart_ai/SmartAiApplication.java
@@ -0,0 +1,13 @@
+package uz.smart_ai.smart_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/smart_ai/smart_ai/SmartAiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.smart_ai.smart_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
