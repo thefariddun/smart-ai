47dc914060eb038cd8974d50c4e6dfdad8515934
thefariddun <thefariddun@gmail.com>
2025-01-11 21:37:41 +0500

    [add]: added roles settings

--- src/main/java/uz/smart_ai/smart_ai/entity/Roles.java
+++ src/main/java/uz/smart_ai/smart_ai/entity/Roles.java
@@ -2,27 +2,67 @@ package uz.smart_ai.smart_ai.entity;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 import uz.smart_ai.smart_ai.enums.UserRoles;
+
+import java.time.LocalDateTime;
 
 @Table(name = "roles")
 @Entity
-@Getter
-@Setter
 public class Roles {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "profile_id")
     private Long profileId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @JoinColumn(name = "profile_id", insertable = false, updatable = false)
     private User user;
 
     @Enumerated(EnumType.STRING)
     private UserRoles role;
 
+    private LocalDateTime createdDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(Long profileId) {
+        this.profileId = profileId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public UserRoles getRole() {
+        return role;
+    }
+
+    public void setRole(UserRoles role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
 }
